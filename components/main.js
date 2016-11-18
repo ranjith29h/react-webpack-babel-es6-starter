fa79ab0936fac7c784142fb18c7be6404189460a
ranjith29h <ranjith29h@gmail.com>
2016-11-18 17:43:01 +0530

    Update main.js

--- components/main.js
+++ components/main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1 class="title">Hello Test World</h1>, document.getElementById('example'));
+ReactDOM.render(<h1 className="title">Hello Test World</h1>, document.getElementById('example'));
